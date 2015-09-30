63646dc9e9d68858193ffb3fb769329cd087660d
suesm27 <suesm27@gmail.com>
2015-09-30 13:01:39 -0700

    minor css changes

--- client/javascripts/loading.js
+++ client/javascripts/loading.js
@@ -53,7 +53,7 @@ var backgrounds = ['#2A3542'];
 // Spinners from SpinKit
 //   https://github.com/tobiasahlin/SpinKit/blob/master/css/spinkit.css
 //
-var spinners2 = [
+var spinners = [
   // spinner-rotating-plane
   '<div class="sk-spinner sk-spinner-rotating-plane"></div>',
 
@@ -144,20 +144,3 @@ var spinners2 = [
   + '  <div class="sk-circle12 sk-circle"></div>'
   + '</div>'
 ];
-
-var spinners = [
-  '<div class="sk-spinner sk-spinner-fading-circle">'
-  + '  <div class="sk-circle1 sk-circle"></div>'
-  + '  <div class="sk-circle2 sk-circle"></div>'
-  + '  <div class="sk-circle3 sk-circle"></div>'
-  + '  <div class="sk-circle4 sk-circle"></div>'
-  + '  <div class="sk-circle5 sk-circle"></div>'
-  + '  <div class="sk-circle6 sk-circle"></div>'
-  + '  <div class="sk-circle7 sk-circle"></div>'
-  + '  <div class="sk-circle8 sk-circle"></div>'
-  + '  <div class="sk-circle9 sk-circle"></div>'
-  + '  <div class="sk-circle10 sk-circle"></div>'
-  + '  <div class="sk-circle11 sk-circle"></div>'
-  + '  <div class="sk-circle12 sk-circle"></div>'
-  + '</div>'
-]
