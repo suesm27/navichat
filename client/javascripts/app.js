cf519b51ef9469d8a67c2461dfc8ad33e3e5272e
lannng <lannguyen309@gmail.com>
2015-10-01 14:44:29 -0700

    removed duplicated files. fixed conflicts

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -193,13 +193,6 @@ Template.profile.helpers({
   }
 });
 
-Template.dashboard.events ({
-  "submit #profileForm": function (event) {
-    event.preventDefault();
-    var data = event.target;
-    Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.name": data.name.value, "profile.occupation": data.occupation.value, "profile.location": data.location.value, "profile.birthdate": data.birthdate.value, "profile.phone": data.phone.value, "profile.description": data.description.value}});
-});
-
 Template.dashboard.helpers({
   name: function () {
     return Meteor.users.findOne({_id: Meteor.userId()}).profile.name;
