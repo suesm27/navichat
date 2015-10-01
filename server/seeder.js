860cab7392b06d696887d53f4ab4c14e6597920c
suesm27 <suesm27@gmail.com>
2015-09-30 17:27:21 -0700

    removed fake message seeding

--- server/seeder.js
+++ server/seeder.js
@@ -2,22 +2,22 @@ Meteor.startup(function() {
   Messages.remove({});
   Chatrooms.remove({});
 
-  var user = Meteor.users.findOne();
-  if(typeof user !== "undefined"){
-    Factory.define('message', Messages, {
-      text: function() {
-        return Fake.sentence();
-      },
-      user: Meteor.users.findOne()._id,
-      timestamp: Date.now(),
-      chatroom: 'San Jose'
-    });
-    if (Messages.find({}).count() === 0) {
-      _(5).times(function(n) {
-        Factory.create('message');
-      });
-    }
-  }
+  // var user = Meteor.users.findOne();
+  // if(typeof user !== "undefined"){
+  //   Factory.define('message', Messages, {
+  //     text: function() {
+  //       return Fake.sentence();
+  //     },
+  //     user: Meteor.users.findOne()._id,
+  //     timestamp: Date.now(),
+  //     chatroom: 'San Jose'
+  //   });
+  //   if (Messages.find({}).count() === 0) {
+  //     _(5).times(function(n) {
+  //       Factory.create('message');
+  //     });
+  //   }
+  // }
 
   Chatrooms.insert({
     name: "San Jose"
