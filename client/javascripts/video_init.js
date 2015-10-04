d29051786182dec4dbcacf917c301425a7a05fe3
suesm27 <suesm27@gmail.com>
2015-10-03 21:51:33 -0700

    now only prompting user for camera access on video call page

--- client/javascripts/video_init.js
+++ client/javascripts/video_init.js
@@ -1,31 +1,24 @@
-if (Meteor.isClient) {
-
+Template.video_chat.rendered = function(){
   var new_video = new video();
   new_video.run();
+};
 
-  Template.call_other.events({
-    'click .callaction' : function(e, t) {
-      console.log("calling");
-      e.preventDefault();
-      var key = t.find('.callkey').value;
-      new_video.callAKey(key);
-      return false;
-    }
-  });
 
-  Template.hangup.events({
-    'click .hangupaction' : function(e, t) {
-      e.preventDefault();
-      new_video.hangup();
-      return false;
-    }
-  });
+Template.call_other.events({
+  'click .callaction' : function(e, t) {
+    console.log("calling");
+    e.preventDefault();
+    var key = t.find('.callkey').value;
+    new_video.callAKey(key);
+    return false;
+  }
+});
+
+Template.hangup.events({
+  'click .hangupaction' : function(e, t) {
+    e.preventDefault();
+    new_video.hangup();
+    return false;
+  }
+});
   
-}
-
-if (Meteor.isServer)
-{
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
