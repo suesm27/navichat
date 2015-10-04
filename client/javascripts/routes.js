f6d9231cce5b442565366685681d2b638134109b
suesm27 <suesm27@gmail.com>
2015-10-04 01:29:13 -0700

    fixed iron router layout error

--- client/javascripts/routes.js
+++ client/javascripts/routes.js
@@ -4,26 +4,35 @@ Router.configure({
 });
 
 Router.route('/', function () {
-    // this.redirect('/chatrooms/San Jose');
     this.render('instruction');
+},{
+	layoutTemplate: 'app' 
 });
 
 Router.route('/chatrooms/:chatroom', function () {
 	Session.set('currentWindow', this.params.chatroom);
 	this.render('messages');
+},{
+	layoutTemplate: 'app' 
 });
 
 Router.route('/users/profile/:user', function () {
 	Session.set('currentWindow', this.params.user);
 	this.render('profile');
+},{
+	layoutTemplate: 'app' 
 });
 
 Router.route('/users/dashboard/:user', function () {
 	Session.set('currentWindow', this.params.user);
 	this.render('dashboard');
+},{
+	layoutTemplate: 'app' 
 });
 
 Router.route('/video_chat', function () {
 	Session.set('currentWindow', this.params.user);
 	this.render('video_chat');
+},{
+	layoutTemplate: 'app' 
 });
